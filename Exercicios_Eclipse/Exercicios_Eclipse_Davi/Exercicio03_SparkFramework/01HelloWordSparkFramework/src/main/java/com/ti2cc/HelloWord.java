61824a7a8357d1dae184f8896740f2b138c13a45
DavyKandido <davicandidopucminas@gmail.com>
2024-08-26 18:39:12 -0300

    ti 1 e ti 2

---
+++ Exercicios_Eclipse/Exercicios_Eclipse_Davi/Exercicio03_SparkFramework/01HelloWordSparkFramework/src/main/java/com/ti2cc/HelloWord.java
@@ -0,0 +1,12 @@
+package com.ti2cc;
+
+import static spark.Spark.*;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		port(5678);
+		get("/hello", (request, response) -> "Hello Word!");
+	}
+
+}
